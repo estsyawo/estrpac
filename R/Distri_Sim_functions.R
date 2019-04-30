26a58fbe69193c1e06ceec37f7321a1af24422e6
Emmanuel Tsyawo <emmantsygarv@hotmail.com>
2019-04-29 22:40:11 -0400

    Update,HS18 option, indepMHgen

--- R/Distri_Sim_functions.R
+++ R/Distri_Sim_functions.R
@@ -271,8 +271,10 @@ predspcj<- function(j,dat,useW = T){
 #' @param propob a list of mode and variance-covariance matrix of the normal proposal distribution. 
 #' Save list as propob=list(mode=mode,var=variance-covariance)
 #' @param const a vector function of parameters showing non-negative inequality constraints to be satisfied. 
-#' @param useed a logical to use seed for reproducibility or not. Default is true
-#' @param scale a value multiplied by \code{propob$var} in order to adjust the proposal distribution.
+#' @param seed an integer as seed for reproducibility
+#' @param scale a value multiplied by \code{propob$var} in order to adjust the proposal distribution. Else
+#' set to the character string "HS18" for the Herbst and Shorfheide (2018) scale updating for a 0.25
+#' acceptance ratio.
 #' The default is \code{1.5} but the user can adjust it until a satisfactory acceptance rate is obtained.
 #' @param iter number of random draws desired (default: 15000)
 #' @param burn burn-in period for the MH algorithm (default: floor(iter/10))
@@ -290,7 +292,7 @@ predspcj<- function(j,dat,useW = T){
 #' # laplace approximation of the posterior
 #' propob = list(mode=optp$par,var=-solve(optp$hessian)) #parameters of proposal distribution
 #' eigen(propob$var)$values # var-cov of proposal distribution is positive definite
-#' MHobj<- indepMHgen(posterior = logpost,propob = propob,scale = 2,iter = 6000)
+#' MHobj<- indepMHgen(posterior = logpost,propob = propob,scale = "HS18",iter = 6000)
 #' # create an independent Metropolis-Hastings object
 #' dim(MHobj$Matpram) # a 2 x 5000 matrix with columns corresponding to draws of c1 and c2
 #' par(mfrow=c(1,2))
@@ -301,23 +303,27 @@ predspcj<- function(j,dat,useW = T){
 #' @export
 
 indepMHgen<- function(start=NULL,posterior=NULL,...,propob=NULL,const=NULL,
-                      useed=TRUE,scale=1.5,iter=5000,burn=floor(0.1*iter),
+                      seed=1,scale=1.5,iter=5000,burn=floor(0.1*iter),
                       report=FALSE){
-  varprop = scale*propob$var
+  if(scale!="HS18"){
+    varprop = scale*propob$var
+  }else{
+    varprop = propob$var
+    c0 = 1.0 #initialise adapting scale
+    }
+  if(!is.null(seed)){set.seed(seed = seed)}
   npar = length(propob$mode)
   Mat = array(0, c(iter, npar)); postvals<- c(0)
 
   if(is.null(const))
   {  
     if(is.null(start)){
-      if(useed){set.seed(iter)}
       start = MASS::mvrnorm(n=1,propob$mode,varprop)
     }
     Mat[1,] = start; AccptRate<-0; postvals[1]<- posterior(start,...)
     
     for(i in 2:iter){
     start= Mat[i-1,]
-    if(useed){set.seed(i)}
     prop = MASS::mvrnorm(n=1,propob$mode,varprop)#make a draw from proposal dist
     lpa = posterior(prop,...); lpb = posterior(start,...)
     accprob = exp(lpa-lpb)
@@ -331,11 +337,15 @@ indepMHgen<- function(start=NULL,posterior=NULL,...,propob=NULL,const=NULL,
       Mat[i,]=start 
       postvals[i]<- postvals[i-1]
     }
+    if(scale=="HS18"){
+      rx = AccptRate/i #acceptance ratio so far
+      c1 = c0*(0.95 + 0.1*exp(16*(rx-0.25))/(1+exp(16*(rx-0.25))))
+      c0 = c1
+    }
   }
   }else{
     if(is.null(start)){
       cnt<- 0
-      if(useed){set.seed(iter+4)}
       start = MASS::mvrnorm(n=1,propob$mode,varprop)
       while(any(const(start)<0)){
         start = MASS::mvrnorm(n=1,propob$mode,varprop) ; cnt<- cnt+1
@@ -346,7 +356,6 @@ indepMHgen<- function(start=NULL,posterior=NULL,...,propob=NULL,const=NULL,
     
     for(i in 2:iter){
       start= Mat[i-1,]
-      if(useed){set.seed(i)}
       prop = MASS::mvrnorm(n=1,propob$mode,varprop)#make a draw from proposal dist
       cnt<- 0
       while(any(const(prop)<0)){
@@ -364,6 +373,11 @@ indepMHgen<- function(start=NULL,posterior=NULL,...,propob=NULL,const=NULL,
       }else{
         Mat[i,]=start 
         postvals[i]<- postvals[i-1]
+      }
+      if(scale=="HS18"){
+        rx = AccptRate/i #acceptance ratio so far
+        c1 = c0*(0.95 + 0.1*exp(16*(rx-0.25))/(1+exp(16*(rx-0.25))))
+        c0 = c1
       }
     }
   }
