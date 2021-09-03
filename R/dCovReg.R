994333cb21eaef4889144fea31bf0f9ae7e59a92
Emmanuel Tsyawo <emmantsygarv@hotmail.com>
2021-09-03 12:09:46 +0100

    updates, codes MMD, generic function for several integrated IV methods

--- R/dCovReg.R
+++ R/dCovReg.R
@@ -1,13 +1,14 @@
 #==========================================================================================>
 #' MMD Regression
 #'
-#' \code{mmdreg} is runs a linear minimum mean dependence (MMD) regression.
+#' \code{mmdreg} runs a linear minimum mean dependence (MMD) regression.
 #'
 #' @param Y outcome variable
 #' @param X matrix of covariates.
 #' @param Z matrix of instruments. Defaults to \code{X}.
 #' @param cl number of clusters to pass to \code{pbsapply()}. This is only advised in large samples.
-#' @return an IV regression object which also contains coefficients, standard errors, etc.
+#' @return an IV regression object which also contains coefficients, standard errors, etc. The
+#' standard errors are computed based on a U-Statistics characterisation of the influence function
 #' 
 #' @importFrom stats dist
 #' 
@@ -21,7 +22,7 @@
 #' @export
 
 mmdreg = function(Y,X,Z=X,cl=NULL){
-  YY = Y - mean(Y);XX=X; for (k in 1:ncol(X)){XX[,k]=X[,k]-mean(X[,k])}; n = length(Y)
+  YY = Y - mean(Y);XX=X=as.matrix(X); for (k in 1:ncol(XX)){XX[,k]=X[,k]-mean(X[,k])}; n = length(Y)
   #in case Z is already a Euclidean distance matrix
   Z = as.matrix(Z)
   if(!(ncol(Z)==nrow(Z))){Mz = as.matrix(dist(Z))}else{Mz=Z}
@@ -30,7 +31,7 @@ mmdreg = function(Y,X,Z=X,cl=NULL){
   if(is.null(cl)){Zhat = t(sapply(1:n,fn))}else{Zhat = t(pbapply::pbsapply(1:n,fn,cl=cl))}
   if(nrow(Zhat)!=n){Zhat=t(Zhat)} #in case row instead of column matrix
   Zhat = Zhat*n/(n-1) #U-statistic correction
-  obj=AER::ivreg(YY~as.matrix(XX)|as.matrix(Zhat))
+  obj=AER::ivreg(YY~as.matrix(XX)|as.matrix(Zhat),x=TRUE)
   fn = function(i) sum(c(Mz[i,])*obj$residuals)/(n-1)
   if(is.null(cl)){Uhat = t(sapply(1:n,fn))}else{Uhat = t(pbapply::pbsapply(1:n,fn,cl=cl))}
   if(nrow(Uhat)!=n){Uhat=t(Uhat)} #in case row instead of column matrix
@@ -38,6 +39,43 @@ mmdreg = function(Y,X,Z=X,cl=NULL){
   VC = solve(A,solve(A,B))/n
   obj$MMD_VC = VC; obj$MMD_SE = sqrt(diag(VC)); obj$MMD_coefficients=obj$coefficients[-1]
   obj$MMD_tstat=obj$MMD_coefficients/obj$MMD_SE; obj$MMD_Zm=Mz
+  obj$MMD_Z=Z;class(obj)=c("MMD",class(obj))
+  obj
+}
+#==========================================================================================>
+
+
+#==========================================================================================>
+#' MMD Regression
+#'
+#' \code{imreg} runs a linear integrated moment regression.
+#'
+#' @param Y outcome variable
+#' @param X matrix of covariates.
+#' @param Z matrix of instruments. Defaults to \code{X}.
+#' @param Kern type of kernel. See \code{\link{Kern.fun}} for available kernels
+#' @return an IV regression object which also contains coefficients, standard errors, etc.
+#' 
+#' @importFrom stats dist
+#' @importFrom sandwich vcovHC
+#' @importFrom AER ivreg
+#' 
+#' @examples 
+#' ## Generate data and run MMD regression
+#' n=200; set.seed(12); X = rnorm(n); er = rchisq(n,df=1)-1; Z=X; X=scale(abs(X))+er/sqrt(2)
+#' Y=X+er
+#' reg = imlmreg(Y,X,Z) #run regression
+#' AER::ivreg(formula = Y ~ X | Z) #compare to conventional IV regression
+#' @export
+
+imlmreg = function(Y,X,Z=X,Kern="Euclid"){
+  YY = Y - mean(Y);XX=X=as.matrix(X); for (k in 1:ncol(XX)){XX[,k]=X[,k]-mean(X[,k])}; n = length(Y)
+  Z = as.matrix(Z) #in case Z is already a Euclidean distance matrix
+  if(!(ncol(Z)==nrow(Z)&isSymmetric(Z))){Mz = Kern.fun(Z,Kern)}else{Mz=Z}
+  Zhat = Mz%*%XX/n
+  obj=AER::ivreg(YY~as.matrix(XX)|as.matrix(Zhat))
+  obj$vcovHC=sandwich::vcovHC(obj)
+  obj$HC_Std.Err=sqrt(diag(obj$vcovHC))
   obj
 }
 #==========================================================================================>
@@ -50,14 +88,13 @@ mmdreg = function(Y,X,Z=X,cl=NULL){
 #' possibly multivariate Z. The MDD measures the mean dependence of U on Z.
 #' 
 #' @param U the univariate variable
-#' @param Z the possibly conditioning multivariate variable
+#' @param Z the possibly multivariate conditioning variable
 #' @return the MDD coefficient
 #' 
 #' @importFrom stats dist
 #' 
 #' @examples 
-#' set.seed(12); X = rnorm(200); MDD(X,X^2/sqrt(2))
-#' set.seed(12); Y = rchisq(200,1)/sqrt(2); MDD(Y,X)
+#' set.seed(12); X = rnorm(200); MDD(X,abs(X)); MDD(abs(X),X)
 #' @export
 MDD<- function(U,Z){
   n = length(U)
@@ -68,10 +105,79 @@ MDD<- function(U,Z){
     n=n #dummy operation
   }else{Z = as.matrix(dist(Z))}
   if(!all(n==c(dim(A),dim(Z)))){stop("mismatch in dimensions")}
-  -sum(A*Z)/n
+  -sum(A*Z)/n^2
 }
 #==========================================================================================>
 
+#==========================================================================================>
+#' Construction of n x n Kernel Matrices
+#' 
+#' \code{Kern.fun} computes the n x n matrix of kernels used in the integrated moment class
+#' of estimators. 
+#' 
+#' @param Z n x p matrix of instrumental variables
+#' @param Kern type of kernel desired. 
+#' @details The \eqn{(i,j)}'th elements of available kernel methods are
+#' \describe{
+#' \item{"Euclid"}{Euclidean distance between two vectors: ||Z_i-Z_j||} 
+#' \item{"Gauss.W"}{The weighted Gaussian kernel: exp(-0.5(Z_i-Z_j)'V^{-1}(Z_i-Z_j)) where V is the variance of V} 
+#' \item{"Gauss"}{The unweighted Gaussian kernel: exp(-||Z_i-Z_j||^2)}
+#' \item{"DL"}{The kernel of Dominguez & Lobato 2004: \eqn{1/n\sum{l=1}^n I(Z_i\le Z_l)I(Z_j\le Z_l)}}
+#' }
+#'  
+#' @return the MDD coefficient
+#' 
+#' @importFrom stats dist
+#' @importFrom expm sqrtm
+#' 
+#' @examples 
+#' set.seed(12); X = rnorm(5); Z=cbind(X,abs(X)); Kern.fun(Z,"DL")
+#' @export
+Kern.fun = function(Z,Kern="Euclid"){
+  Z=as.matrix(Z); n = nrow(Z)
+  if(Kern=="Euclid"){Omg=as.matrix(dist(Z))}
+  else if(Kern=="Gauss.W"){
+    Omg=exp(-0.5*as.matrix(dist(Z%*%expm::sqrtm(solve(cov(Z)))))^2)}
+  else if(Kern=="Gauss"){Omg=exp(-0.5*as.matrix(dist(Z))^2)}
+  else if(Kern=="DL"){
+    Omg=Om=matrix(NA,n,n)
+    fn=function(i){ 
+      sub.fn=function(j){all(Z[i,]<=Z[j,])*1}; sub.fn=Vectorize(sub.fn)
+      sub.fn(1:nrow(Z))}#end function fn
+    for(i in 1:n){Om[i,]=fn(i)}
+    for(i in 1:n){for(j in 1:i){Omg[i,j]=mean(Om[i,]*Om[,j]);Omg[j,i]=Omg[i,j]}}
+  }else{stop(cat("The method ",Kern," is not available."))}
+  return(Omg)
+}
+
+#==========================================================================================>
+
+
+#==========================================================================================>
+#' Martingale Difference Divergence Matrix
+#' 
+#' \code{MDDM} computes the Martingale Difference Divergence Matrix between multivariate U and 
+#' possibly multivariate Z of Lee & Shao 2018.
+#' 
+#' @param U the multivariate variable
+#' @param Z the possibly multivariate conditioning variable. One can also directly supply
+#' an n x n distance matrix
+#' @return the MDDM matrix
+#' 
+#' @importFrom stats dist
+#' 
+#' @examples 
+#' set.seed(12); U = rnorm(50); U=cbind(U,abs(U)); MDDM(U,2*pnorm(U[,1]))
+#' MDDM(U,as.matrix(dist(2*pnorm(U[,1]))))
+#' @export
+MDDM<- function(U,Z){
+  U = as.matrix(U); ncU=ncol(U); Z = as.matrix(Z)
+  if(ncU<=1){stop("U must be multivariate.")}
+  for (j in 1:ncU) {U[,j] = U[,j]-mean(U[,j])}
+  if(ncol(Z)!=nrow(Z)){Z=as.matrix(dist(Z))}
+  -crossprod(U,Z)%*%U/(nrow(U)^2)
+}
+#==========================================================================================>
 
 
 #==========================================================================================>
@@ -105,12 +211,11 @@ wmat.mammen<- function(n,B=200,seed=NULL,cluster=NULL){
 #==========================================================================================>
 
 
-
 #==========================================================================================>
 #' MMD Specification Test
 #'
-#' \code{mmd.lmspec_test} conducts the Su \& Zheng 2017 specification test a linear model
-#' estimated with the MMD estimator.
+#' \code{mmdlmspec.b_test} conducts the Su \& Zheng 2017 specification test on a linear model
+#' estimated with the MMD estimator. It is based on the wild bootstrap.
 #'
 #' @param mmd.Obj is an MMD regression output from mmdreg()
 #' @param B number of wild bootstrap samples. Defaults to 199
@@ -124,23 +229,66 @@ wmat.mammen<- function(n,B=200,seed=NULL,cluster=NULL){
 #' ## Generate data and run MMD regression
 #' n=200; set.seed(12); X = rnorm(n); er = rchisq(n,df=1)-1; Z=X; X=scale(abs(X))+er/sqrt(2)
 #' Y=X+er; reg1 = mmdreg(Y,X,Z); reg2 = mmdreg(Y,X,X) #run regression
-#' mmd.lmspec_test(reg1,B=99); mmd.lmspec_test(reg2,B=99) #test under the null and the alternative
+#' mmdlmspec.b_test(reg1); mmdlmspec.b_test(reg2) #test under the null and the alternative
 #' ## MMD coefficients, standard errors, and t-statistics
 #' 
 #' @export
 
-mmd.lmspec_test<- function(mmd.Obj,B=199,wmat=NULL,cl=NULL,cluster=NULL){#returns p-value
+mmdlmspec.b_test<- function(mmd.Obj,B=199,wmat=NULL,cl=NULL,cluster=NULL){#returns p-value
   X = mmd.Obj$model[[2]]; k = ncol(X); n = nrow(X)
-  Tn_SZo = MDD(mmd.Obj$residuals,Z=mmd.Obj$MMD_Zm)
+  Tn_SZo = n*MDD(mmd.Obj$residuals,Z=mmd.Obj$MMD_Zm)
   if(is.null(wmat)){wmat = wmat.mammen(n=n,B=B,seed = n,cluster=cluster)}
   
   fn<- function(j){
     Ystar = mmd.Obj$fitted.values + sqrt(n/(n-k-1))*mmd.Obj$residuals*wmat[,j]
-    MDD(mmdreg(Ystar,X=X,Z=mmd.Obj$MMD_Zm)$residuals,mmd.Obj$MMD_Zm)
+    n*MDD(mmdreg(Ystar,X=X,Z=mmd.Obj$MMD_Zm)$residuals,mmd.Obj$MMD_Zm)
   }
   if(is.null(cl)){TnSZ_boot=sapply(1:B,fn)}else{TnSZ_boot=pbapply::pbsapply(1:B,fn,cl=cl)}
-  list(p_value=mean(TnSZ_boot>Tn_SZo),Tn=Tn_SZo)
+  list(statistic=Tn_SZo,p.value=mean(TnSZ_boot>Tn_SZo))
 }
+#==========================================================================================>
+
+
+#==========================================================================================>
+#' MMD Relevance Test
+#'
+#' \code{mmdlmrelv.b_test} conducts the Su \& Zheng 2017 specification test on a linear model
+#' estimated with the MMD estimator. It is based on the wild bootstrap.
+#'
+#' @param X1 an n x p1 matrix of endogenous covariates
+#' @param X2 an n x p2 matrix of exogenous covariates
+#' @param Z an n x pz matrix of instruments
+#' @param B number of wild bootstrap samples. Defaults to 199
+#' @param wmat in case the n x B matrix of wild bootstrap weights are supplied by the user.
+#' @param cl an integer to indicate number of child-processes in pbapply::pbsapply().
+#' @param cluster vector of length n with cluster ids if cluster-robust wild-bootstrap is
+#'  used
+#' @return the p-value, test statistic, 
+#' 
+#' @examples 
+#' n=200; set.seed(12); X = rnorm(n); er = (rchisq(n,df=1)-1)/sqrt(2)
+#' X1=(X+er)/sqrt(2);mmdlmrelv.b_test(X1,abs(X),X^3);mmdlmrelv.b_test(X1,X,X^3)
+#' 
+#' @export
+
+mmdlmrelv.b_test<- function(X1,X2=NULL,Z,B=199,wmat=NULL,cl=NULL,cluster=NULL){
+  X1=as.matrix(X1); p1 = ncol(X1)
+  if(p1==1){
+    if(!is.null(X2)){X2=as.matrix(X2) #exogenous covariates
+    mmd.Obj = mmdreg(X1,X2,Z)
+    ans=mmdlmspec.b_test(mmd.Obj,B=B,wmat=wmat,cl=cl,cluster=cluster)
+    }else{#no exogenous covariates
+      ans=mddtest.boot(X1,Z,B=B,wmat=wmat,cl=cl,cluster=cluster)
+    }#end if(!is.null(X2))
+  }else{#multiple endogenous covariates
+    #...
+  }#end if(p1==1)
+  
+  return(ans)
+}
+#==========================================================================================>
+
+
 #==========================================================================================>
 #' MDep objective function for non-linear models
 #'
@@ -169,6 +317,8 @@ mdep.nl=function(theta,U.fun,Z=NULL,Z.m=NULL,sc=1){
   if(is.null(Z.m)){Mat=energy::U_center(as.matrix(stats::dist(Z)));Z.m = c(Mat[lower.tri(Mat)])}
   sc*mean(c(dist(U.fun(theta)))*Z.m)
 }
+#==========================================================================================>
+
 
 #==========================================================================================>
 #' MMD objective function for non-linear models
@@ -199,4 +349,102 @@ mmd.nl=function(theta,U.fun,Z=NULL,Z.m=NULL,sc=1){
   U = U.fun(theta); U=U-mean(U); U.m = tcrossprod(U)
   -sc*mean(c(U.m[lower.tri(U.m)])*Z.m)
 }
+#==========================================================================================>
 
+
+#==========================================================================================>
+#' An MDD-based Test of Mean Independence by Wild Bootstrap
+#'
+#' \code{mddtest.boot} tests the mean independence of U conditional on V using the wild
+#' bootstrap following Shao & Zhang 2014.
+#'
+#' @param U univariate variable
+#' @param V possibly multivariate variable
+#' @param B number of wild bootstrap samples. Defaults to 199
+#' @param wmat in case the n x B matrix of wild bootstrap weights are supplied by the user.
+#' @param cl an integer to indicate number of child-processes in pbapply::pbsapply().
+#' @param cluster vector of length n with cluster ids if cluster-robust wild-bootstrap is
+#'  used
+#' @return test statistic and p-value
+#' 
+#' @examples 
+#' set.seed(12); X = rnorm(200)
+#' mddtest.boot(X,abs(X)); mddtest.boot(abs(X),X)
+#' @export
+#' 
+mddtest.boot<- function(U,V,B=199,wmat=NULL,cl=NULL,cluster=NULL){
+  U = U-mean(U); n = length(U); V = as.matrix(dist(V))
+  mdd0 = n*MDD(U,V)
+  if(is.null(wmat)){
+    wmat=wmat.mammen(n,B=B,seed=0,cluster=cluster)
+  }
+  fn = function(j) {Ustar=U*wmat[,j]; n*MDD(Ustar,V)}
+  if(is.null(cl)){Tns=sapply(1:B, fn)}else{Tns=pbapply::pbsapply(1:B, fn,cl=cl)}
+  ans=list()
+  ans$statistic = mdd0; ans$p.value=(1+sum(Tns>ans$statistic))/(1+B)
+  ans
+}
+#==========================================================================================>
+
+
+#==========================================================================================>
+#' An MDD-based Test of Mean Independence by Permutation Bootstrap
+#'
+#' \code{mddtest.boot} tests the mean independence of U conditional on V using the wild
+#' bootstrap following Shao & Zhang 2014.
+#'
+#' @param U univariate variable
+#' @param V possibly multivariate variable
+#' @param B number of wild bootstrap samples. Defaults to 199
+#' @param PBmat in case the n x B matrix of permuted ids is supplied by the user.
+#' @param cl an integer to indicate number of child-processes in pbapply::pbsapply().
+#'  used
+#' @return test statistic and p-value
+#' 
+#' @examples 
+#' set.seed(12); X = rnorm(200)
+#' mddtest.perm(X,abs(X)); mddtest.perm(abs(X),X)
+#' @export
+#' 
+mddtest.perm<- function(U,V,B=199,PBmat=NULL,cl=NULL){
+  U = U-mean(U); n = length(U); V = as.matrix(dist(V))
+  mdd0 = n*MDD(U,V)
+  if(is.null(PBmat)){
+    PBmat=matrix(NA,n,B); for(j in 1:B){set.seed(j);PBmat[,j]=sample(1:n,n,replace = FALSE)}
+  }
+  fn = function(j) {n*MDD(U[PBmat[,j]],V)}
+  if(is.null(cl)){Tns=sapply(1:B, fn)}else{Tns=pbapply::pbsapply(1:B, fn,cl=cl)}
+  ans=list()
+  ans$statistic = mdd0; ans$p.value=(1+sum(Tns>ans$statistic))/(1+B)
+  ans
+}
+#==========================================================================================>
+
+
+#==========================================================================================>
+#' An MDD-based t-Test of Mean Independence
+#'
+#' \code{mdd.t_test} tests the mean independence of Y conditional on Z using a two-sided
+#' t-test.
+#'
+#' @param Y univariate variable
+#' @param Z possibly multivariate variable
+#' @return t-statistic, standard error, and p-value of test
+#' 
+#' @importFrom stats pnorm
+#' 
+#' @examples 
+#' set.seed(12); X = rnorm(200)
+#' mdd.t_test(X,abs(X)); mdd.t_test(abs(X),X)
+#' @export
+
+mdd.t_test=function(Y,Z){
+  Z = as.matrix(Z); Y=scale(Y); X = Y + apply(Z,1,mean); X=scale(X) #elicit an endogenous X
+  MMDobj=mmdreg(Y,X,Z)
+  SE_MMD = sqrt(MMDobj$MMD_VC); obj=list()
+  Tstat=MMDobj$coefficients[-1]/SE_MMD
+  obj$statistic=c(Tstat); obj$stderr=c(SE_MMD)
+  obj$p.value=2*(1-pnorm(abs(obj$statistic)))
+  obj
+}
+#==========================================================================================>
